ea1b3d4b1739eb909220560505a87526d1d24ac3
Patrick Pierre <patrickpierre2015@gmail.com>
2025-05-28 17:52:25 -0300

    Feat: adicionado lista e controller de pacientes

---
+++ src/main/java/equipe/unialfa/radioclinica/clinica_medica_unialfa_radioclinica/controller/Agendamento.java
@@ -0,0 +1,4 @@
+package equipe.unialfa.radioclinica.clinica_medica_unialfa_radioclinica.controller;
+
+public class Agendamento {
+}
--- src/main/java/equipe/unialfa/radioclinica/clinica_medica_unialfa_radioclinica/controller/PacienteController.java
+++ src/main/java/equipe/unialfa/radioclinica/clinica_medica_unialfa_radioclinica/controller/PacienteController.java
@@ -1,4 +1,21 @@
 package equipe.unialfa.radioclinica.clinica_medica_unialfa_radioclinica.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("paciente")
 public class PacienteController {
+
+    @GetMapping()
+    public String iniciar(Model model) {
+        return "paciente/formulario";
+    }
+
+    @GetMapping("lista")
+    public String listar(Model model) {
+        return "paciente/lista";
+    }
 }
