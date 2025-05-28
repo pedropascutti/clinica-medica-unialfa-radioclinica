99187aee16c2cbbd2a72ff344e907c7c40960ed3
Pedro Pascutti <pascuttipedro@gmail.com>
2025-05-27 21:49:33 -0300

    initial commit

---
+++ src/main/java/equipe/unialfa/radioclinica/clinica_medica_unialfa_radioclinica/ClinicaMedicaUnialfaRadioclinicaApplication.java
@@ -0,0 +1,13 @@
+package equipe.unialfa.radioclinica.clinica_medica_unialfa_radioclinica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicaMedicaUnialfaRadioclinicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicaMedicaUnialfaRadioclinicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/equipe/unialfa/radioclinica/clinica_medica_unialfa_radioclinica/ClinicaMedicaUnialfaRadioclinicaApplicationTests.java
@@ -0,0 +1,13 @@
+package equipe.unialfa.radioclinica.clinica_medica_unialfa_radioclinica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaMedicaUnialfaRadioclinicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
