d9944d1c7f7397efc4a3fde17f38dae7810b35fe
joaopedro <joaopkrodrigues@gmail.com>
2025-05-28 19:10:34 -0300

    Feat: Preenchendo arquivos model e repository para PACIENTE.

--- src/main/java/equipe/unialfa/radioclinica/clinica_medica_unialfa_radioclinica/repository/PacienteRepository.java
+++ src/main/java/equipe/unialfa/radioclinica/clinica_medica_unialfa_radioclinica/repository/PacienteRepository.java
@@ -1,4 +1,7 @@
 package equipe.unialfa.radioclinica.clinica_medica_unialfa_radioclinica.repository;
 
-public interface PacienteRepository {
+import equipe.unialfa.radioclinica.clinica_medica_unialfa_radioclinica.model.Paciente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PacienteRepository extends JpaRepository<Paciente, Long> {
 }
