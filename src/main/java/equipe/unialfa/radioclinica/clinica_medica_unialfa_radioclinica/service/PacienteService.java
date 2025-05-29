6c01c0094f2cd99ede07eaa75576c9d9360e24aa
Gabriel Paiva de Almeida <gabriel.almeida@pmu.localdomain>
2025-05-29 08:42:23 -0300

    Feat: Iniciando o desenvolvimento do PacienteService. Adicionada as importações necessárias para o arquivo.

--- src/main/java/equipe/unialfa/radioclinica/clinica_medica_unialfa_radioclinica/service/PacienteService.java
+++ src/main/java/equipe/unialfa/radioclinica/clinica_medica_unialfa_radioclinica/service/PacienteService.java
@@ -1,4 +1,14 @@
 package equipe.unialfa.radioclinica.clinica_medica_unialfa_radioclinica.service;
+import equipe.unialfa.radioclinica.clinica_medica_unialfa_radioclinica.model.Paciente;
+import equipe.unialfa.radioclinica.clinica_medica_unialfa_radioclinica.repository.PacienteRepository;
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+    
+import java.util.List;
 
 public class PacienteService {
+
+
 }
