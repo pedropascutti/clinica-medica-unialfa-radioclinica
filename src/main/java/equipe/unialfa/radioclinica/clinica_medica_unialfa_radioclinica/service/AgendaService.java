4edae1614e00edd3e2e8ffd9e6a027a571864bb0
Gabriel Paiva de Almeida <gabriel.almeida@pmu.localdomain>
2025-05-29 13:44:58 -0300

    Feat: Adicionado os códigos para busca por ID para salvar um agendamento.

---
+++ src/main/java/equipe/unialfa/radioclinica/clinica_medica_unialfa_radioclinica/service/AgendaService.java
@@ -0,0 +1,45 @@
+package equipe.unialfa.radioclinica.clinica_medica_unialfa_radioclinica.service;
+
+import equipe.unialfa.radioclinica.clinica_medica_unialfa_radioclinica.model.Agenda;
+import equipe.unialfa.radioclinica.clinica_medica_unialfa_radioclinica.model.Medico;
+import equipe.unialfa.radioclinica.clinica_medica_unialfa_radioclinica.model.Paciente;
+import equipe.unialfa.radioclinica.clinica_medica_unialfa_radioclinica.repository.AgendaRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+public class AgendaService {
+    private final AgendaRepository agendaRepository;
+    private final MedicoService medicoService;
+    private final PacienteService pacienteService;
+
+    // pra listar todos os agendamentos 
+    public List<Agenda> listarTodos() {
+        return agendaRepository.findAll();
+    }
+
+    // buscar por id
+    public Agenda buscarPorId(Long id) {
+        Optional<Agenda> agenda = agendaRepository.findById(id);
+        return agenda.orElseThrow(() -> new RuntimeException("Agendamento não encontrado"));
+
+    }
+    // salvar um agendamento
+    public Agenda salvar(Agenda agenda) {
+        Medico medico = medicoService.buscarPorId(agenda.getMedico().getId()); // TODO: Inserir buscarPorId no MedicoService
+        if (medico == null) {
+            throw new RuntimeException("Médico não encontrado");
+        }
+        Paciente paciente = pacienteService.buscarPorId(agenda.getPaciente().getId());
+        agenda.setMedico(medico);
+        agenda.setPaciente(paciente);
+        return agendaRepository.save(agenda);
+    }
+
+
+}
