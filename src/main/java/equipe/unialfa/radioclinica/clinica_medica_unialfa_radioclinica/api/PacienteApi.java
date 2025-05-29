d92ba6af9ff7f7cacad540b70c7749ab15d38f76
Gabriel Paiva de Almeida <gabriel.almeida@pmu.localdomain>
2025-05-29 08:26:30 -0300

    Iniciado o desenvolviento da API. Adicionando as importações.

--- src/main/java/equipe/unialfa/radioclinica/clinica_medica_unialfa_radioclinica/api/PacienteApi.java
+++ src/main/java/equipe/unialfa/radioclinica/clinica_medica_unialfa_radioclinica/api/PacienteApi.java
@@ -1,4 +1,13 @@
 package equipe.unialfa.radioclinica.clinica_medica_unialfa_radioclinica.api;
+import equipe.unialfa.radioclinica.clinica_medica_unialfa_radioclinica.model.Paciente; 
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import equipe.unialfa.radioclinica.clinica_medica_unialfa_radioclinica.service.PacienteService;
+
+import java.util.List;
+
+
 
 public class PacienteApi {
 }
