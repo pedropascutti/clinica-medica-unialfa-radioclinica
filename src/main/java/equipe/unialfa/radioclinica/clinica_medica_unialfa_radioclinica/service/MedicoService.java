3340d3dd4f50c1a4117e78b7bf797df42457e35d
Gabriel Paiva de Almeida <gabriel.almeida@pmu.localdomain>
2025-05-30 08:50:36 -0300

    Feat: Adicionando métodos atualizar no medicoService e outros

--- src/main/java/equipe/unialfa/radioclinica/clinica_medica_unialfa_radioclinica/service/MedicoService.java
+++ src/main/java/equipe/unialfa/radioclinica/clinica_medica_unialfa_radioclinica/service/MedicoService.java
@@ -14,15 +14,47 @@ import java.util.Optional;
 public class MedicoService {
     private MedicoRepository medicoRepository;
 
+    // isso aqui é pra listar todos os médicos no lindo front ende
     public List<Medico> listarTodos() {
         return medicoRepository.findAll();
     }
 
+    // isso aqui busca o médico por id único na databeise
     public Medico buscarPorId(Long id) {
         Optional<Medico> medico = medicoRepository.findById(id);
         return medico.orElseThrow(() -> new RuntimeException("Médico não encontrado"));
     }
 
+    // isso aqui é pra salvar um médico (meio doido)
+     public Medico salvar(Medico medico) {
+        try {
+            return medicoRepository.save(medico);
+        } catch (RuntimeException re) {
+            throw re;
+        } catch (Exception e) {
+            System.out.println("Erro ao salvar médico: " + e.getMessage());
+            throw new RuntimeException("Erro ao salvar médico: " + e.getMessage());
+        }
+    }
+
     
+    public Medico atualizar(Long id, Medico medicoAtualizado) {
+        try {
+            Medico medicoExistente = buscarPorId(id);
+
+            medicoExistente.setNome(medicoAtualizado.getNome());
+            medicoExistente.setCrm(medicoAtualizado.getCrm());
+            medicoExistente.setEspecialidade(medicoAtualizado.getEspecialidade());
+            
+
+            return medicoRepository.save(medicoExistente);
+        } catch (RuntimeException re) {
+            throw re;
+        } catch (Exception e) {
+            System.out.println("Erro ao atualizar médico: " + e.getMessage());
+            throw new RuntimeException("Erro ao atualizar médico: " + e.getMessage());
+        }
+    }
+
 
 }
